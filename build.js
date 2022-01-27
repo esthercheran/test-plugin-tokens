7f1e227f680df738b3af6187c68783c5a1c1466b
Esther Cheran <dingse.momin@gmail.com>
2022-01-27 20:40:01 +0530

    - Changed sizes/px to use type prop - Added Transform for fontFamilies - Updated github workflow - hsla fix for transparent

--- build.js
+++ build.js
@@ -1,63 +1,90 @@
-const StyleDictionaryPackage = require('style-dictionary');
+const StyleDictionaryPackage = require("style-dictionary");
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
-    name: 'css/variables',
-    formatter: function (dictionary, config) {
-      return `${this.selector} {
-        ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
-      }`
-    }
-  });  
+  name: "css/variables",
+  formatter: function (dictionary, config) {
+    return `${this.selector} {
+        ${dictionary.allProperties
+          .map((prop) => `  --${prop.name}: ${prop.value};`)
+          .join("\n")}
+      }`;
+  },
+});
 
 StyleDictionaryPackage.registerTransform({
-    name: 'sizes/px',
-    type: 'value',
-    matcher: function(prop) {
-        // You can be more specific here if you only want 'em' units for font sizes    
-        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
-    },
-    transformer: function(prop) {
-        // You can also modify the value here if you want to convert pixels to ems
-        return parseFloat(prop.original.value) + 'px';
-    }
-    });
+  name: "sizes/px",
+  type: "value",
+  matcher: function (prop) {
+    // You can be more specific here if you only want 'em' units for font sizes
+    return [
+      "fontSizes",
+      "spacing",
+      "borderRadius",
+      "borderWidth",
+      "sizing",
+    ].includes(prop.type);
+  },
+  transformer: function (prop) {
+    // You can also modify the value here if you want to convert pixels to ems
+    // console.log(prop);
+    return parseFloat(prop.original.value) + "px";
+  },
+});
+
+// Wraps the value of font families in a double-quoted string to make a string literal.
+StyleDictionaryPackage.registerTransform({
+  name: "fontFamily/literal",
+  type: "value",
+  matcher: function (prop) {
+    return ["fontFamilies"].includes(prop.type);
+  },
+  transformer: function (prop) {
+    return `"${prop.original.value}"`;
+  },
+});
 
 function getStyleDictionaryConfig(theme) {
   return {
-    "source": [
-      `tokens/${theme}.json`,
-    ],
-    "platforms": {
-      "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `output/`,
-        "files": [{
-            "destination": `${theme}.css`,
-            "format": "css/variables",
-            "selector": `.${theme}-theme`
-          }]
-      }
-    }
+    source: [`tokens/${theme}.json`],
+    platforms: {
+      web: {
+        transforms: [
+          "attribute/cti",
+          "name/cti/kebab",
+          "sizes/px",
+          "fontFamily/literal",
+        ],
+        buildPath: `output/`,
+        files: [
+          {
+            destination: `${theme}.css`,
+            format: "css/variables",
+            selector: `.${theme}-theme`,
+          },
+        ],
+      },
+    },
   };
 }
 
-console.log('Build started...');
+console.log("Build started...");
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['global', 'dark', 'light'].map(function (theme) {
+["tokens"].map(function (theme) {
+  console.log("\n==============================================");
+  console.log(`\nProcessing: [${theme}]`);
 
-    console.log('\n==============================================');
-    console.log(`\nProcessing: [${theme}]`);
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(theme)
+  );
 
-    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+  StyleDictionary.buildPlatform("web");
 
-    StyleDictionary.buildPlatform('web');
+  console.log("\nEnd processing");
+});
 
-    console.log('\nEnd processing');
-})
-
-console.log('\n==============================================');
-console.log('\nBuild completed!');
+console.log("\n==============================================");
+console.log("\nBuild completed!");
