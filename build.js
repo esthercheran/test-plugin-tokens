4f3832d79010198a5e04b3c9ec44226a2a47c1a9
Esther Cheran <dingse.momin@gmail.com>
2022-01-27 21:01:44 +0530

    Updated build script

--- build.js
+++ build.js
@@ -73,7 +73,7 @@ console.log("Build started...");
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-["tokens"].map(function (theme) {
+["light"].map(function (theme) {
   console.log("\n==============================================");
   console.log(`\nProcessing: [${theme}]`);
 
